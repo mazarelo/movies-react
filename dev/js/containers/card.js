56a95d7ff620f9f7c7ef6df625ab56dc253da372
João Mazarelo <jvmazarelo@gmail.com>
2018-02-24 19:26:32 +0000

    Removes material ui card

--- dev/js/containers/card.js
+++ dev/js/containers/card.js
@@ -1,51 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
-import Button from 'material-ui/Button';
-import Typography from 'material-ui/Typography';
-
-const styles = {
-  card: {
-    width: 345,
-    maxWidth: 345,
-  },
-  media: {
-    height: 200,
-  },
-};
 
 const SimpleMediaCard = (props) => {
   const { classes, item, clickCallback } = props;
   return (
-    <Card className={classes.card}>
-      <CardMedia
-        className={classes.media}
-        image={item.images.fanart}
-        title={item.title}
-      />
-      <CardContent>
-        <Typography variant="headline" component="h2">
-          {item.title}
-        </Typography>
-        <Typography component="p">
-          extra 
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Watch
-        </Button>
-        <Button size="small" color="primary" onClick={()=> clickCallback(item)}>
+    <div className={classes.card}>
+      <div className="header">
+        <figure>
+          <img src={item.images.fanart}/>
+        </figure>
+      </div>
+      <div className="content">
+        <h3>{item.title}</h3>
+      </div>
+      <div class="action-buttons">
+        <button onClick={()=> clickCallback(item)}>
           Info
-        </Button>
-      </CardActions>
-      </Card>
+        </button>
+      </div>
+    </div>
   );
 }
 
-SimpleMediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(SimpleMediaCard);
+export default SimpleMediaCard
